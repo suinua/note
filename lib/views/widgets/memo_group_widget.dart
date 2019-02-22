9a41c08f2383d8354dfbf64e92b02dd8047ae5b9
suinua <maatinosippo@gmail.com>
2019-02-22 18:58:32 +0900

    update
    columnで囲った

--- lib/views/widgets/memo_group_widget.dart
+++ lib/views/widgets/memo_group_widget.dart
@@ -8,41 +8,45 @@ class MemoGroupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Container(
-        height: 170,
-        child: Card(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: <Widget>[
-                    Center(
-                      child: Text(
-                        memoGroup.title,
-                        style: TextStyle(fontSize: 28),
-                        maxLines: 3,
-                      ),
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: Container(
+            height: 170,
+            child: Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        Center(
+                          child: Text(
+                            memoGroup.title,
+                            style: TextStyle(fontSize: 28),
+                            maxLines: 3,
+                          ),
+                        ),
+                        Divider(),
+                      ],
                     ),
-                    Divider(),
-                  ],
-                ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      memoGroup.description,
+                      style: TextStyle(fontSize: 15,color: Colors.black38),
+                      maxLines: 3,
+                    ),
+                  )
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  memoGroup.description,
-                  style: TextStyle(fontSize: 15,color: Colors.black38),
-                  maxLines: 3,
-                ),
-              )
-            ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
