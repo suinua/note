9f3efc57a95b09ee7bcf2703887fb3d33f04e2da
suinua <maatinosippo@gmail.com>
2019-02-22 15:49:33 +0900

    update

--- lib/models/memo_group.dart
+++ lib/models/memo_group.dart
@@ -1,12 +1,34 @@
+import 'package:note/blocs/memos_bloc.dart';
 import 'package:note/models/memo.dart';
 
 class MemoGroup {
   final String key;
   String title;
   String description;
-  List<Memo> memos;
 
-  MemoGroup(this.title, this.description, {this.key, this.memos});
+  MemosBloc _memosBloc;
+  List<Memo> _memos;
+
+  List<Memo> get getAllMemos => _memos;
+
+  void addMemo(Memo memo){
+    _memosBloc.addMemo.add(memo);
+  }
+  void removeMemo(Memo memo){
+    _memosBloc.removeMemo.add(memo);
+
+  }
+  void updateMemo(Memo memo){
+    _memosBloc.updateMemo.add(memo);
+  }
+
+  MemoGroup(this.title, this.description, {this.key, List<Memo> memos = const <Memo>[]}){
+    _memos = memos;
+   _memosBloc = MemosBloc(this.key);
+   _memosBloc.getAllMemos.listen((event){
+     _memos = event;
+   });
+  }
 
   factory MemoGroup.fromMap(Map<String, dynamic> memoGroup) {
     return MemoGroup(
@@ -20,7 +42,7 @@ class MemoGroup {
   Map<String, dynamic> asMap() => {
         'title': title,
         'description': description,
-        'memos': memos.map((memo) => memo.asMap()).toList(),
+        'memos': _memos.map((memo) => memo.asMap()).toList(),
       };
 
   @override
