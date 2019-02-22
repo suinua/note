de39eddf8271384676147c47593a8f3cf5ef21c6
suinua <maatinosippo@gmail.com>
2019-02-22 19:09:14 +0900

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/blocs/memo_groups_bloc_provider.dart';
-import 'package:note/views/pages/page_memo_groups.dart';
+import 'package:note/views/pages/memo_groups_page.dart';
 
 void main() => runApp(MemoGroupsBlocProvider(child: NoteApp()));
 
