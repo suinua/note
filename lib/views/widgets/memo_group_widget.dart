01913bb6370f5e6129171c8057045d0652f8d386
suinua <maatinosippo@gmail.com>
2019-02-27 17:13:33 +0900

    余分なウィジェットを削除

--- lib/views/widgets/memo_group_widget.dart
+++ lib/views/widgets/memo_group_widget.dart
@@ -9,7 +9,6 @@ class MemoGroupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO : ネストしすぎ
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -21,47 +20,38 @@ class MemoGroupWidget extends StatelessWidget {
       },
       child: Container(
         height: 170,
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: Container(
-                height: 170,
-                child: Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Center(
-                              child: Text(
-                                memoGroup.title,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(fontSize: 25),
-                                maxLines: 2,
-                              ),
-                            ),
-                            Divider(),
-                          ],
-                        ),
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        memoGroup.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 25),
+                        maxLines: 2,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20.0),
-                        child: Text(
-                          memoGroup.description,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 15, color: Colors.black38),
-                          maxLines: 3,
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                    Divider(),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  memoGroup.description,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 15, color: Colors.black38),
+                  maxLines: 3,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
