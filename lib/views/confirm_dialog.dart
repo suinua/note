4324d18b7356041c9f1e1cb41c24c6c5ca0c4032
suinua <maatinosippo@gmail.com>
2019-03-03 15:42:46 +0900

    bodyをセットできるように

--- lib/views/confirm_dialog.dart
+++ lib/views/confirm_dialog.dart
@@ -4,12 +4,13 @@ class ConfirmDialog {
   //TODO : on approvedってなんや、、候補「onOk(？？), onAgreement(まぁ、、), onDecided(cancelの可能性もでてくる、、)」
   //Cancel時は何もしない
 
-  static void show(BuildContext context, {@required String title,@required Function onApproved}) {
+  static void show(BuildContext context, {@required String title, String body = '',@required Function onApproved}) {
     showDialog(
       context: context,
       builder: (_) {
         return AlertDialog(
           title: Text(title),
+          content: Text(body),
           actions: <Widget>[
             FlatButton(
                 child: const Text('Cancel'),
