2d1e99f4e5c2e1576428f829ee3d866d9fff678d
suinua <maatinosippo@gmail.com>
2019-03-04 16:43:28 +0900

    不要なコードを削除

--- lib/views/pages/memo_detail/main.dart
+++ lib/views/pages/memo_detail/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/models/memo.dart';
+import 'package:note/views/pages/memo_detail/editing_widget.dart';
 import 'package:note/views/pages/memo_detail/preview_widget.dart';
 
 class _DisplayMode {
@@ -55,72 +56,6 @@ class _MemoDetailPageState extends State<MemoDetailPage> {
       body: _displayMode.isPreview
           ? MemoPreviewWidget(memo: widget.memo)
           : EditingMemoWidget(memo: widget.memo),
-    );
-  }
-}
-
-class EditingMemoWidget extends StatefulWidget {
-  final Memo memo;
-
-  const EditingMemoWidget({Key key, this.memo}) : super(key: key);
-
-  @override
-  _EditingMemoWidgetState createState() => _EditingMemoWidgetState();
-}
-
-class _EditingMemoWidgetState extends State<EditingMemoWidget> {
-  //初期値用
-  TextEditingController _memoTitleController;
-  TextEditingController _memoBodyController;
-
-  @override
-  void initState() {
-    _memoTitleController = TextEditingController(text: widget.memo.title);
-    _memoBodyController = TextEditingController(text: widget.memo.body);
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          style: TextStyle(fontSize: 30),
-          controller: _memoTitleController,
-          onChanged: (value) {
-            setState(() {
-              widget.memo.title = value;
-            });
-          },
-          decoration: InputDecoration(
-            hintText: 'Title',
-            border: InputBorder.none,
-          ),
-        ),
-        Divider(),
-        Padding(padding: const EdgeInsets.only(bottom: 30.0)),
-        Expanded(
-          child: TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            controller: _memoBodyController,
-            style: TextStyle(fontSize: 25),
-            onChanged: (value) {
-              setState(() {
-                widget.memo.body = value;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: 'Body',
-              border: InputBorder.none,
-            ),
-          ),
-        ),
-        Divider(),
-        //MEMO : キーボードで文字が隠れるのを防ぐため
-        Padding(padding: const EdgeInsets.only(bottom: 40.0)),
-      ],
     );
   }
 }
