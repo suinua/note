d77a7b15ac64b5c108135518fd42b9e42ea18f7c
suinua <maatinosippo@gmail.com>
2019-03-04 17:31:46 +0900

    labelに対応

--- lib/models/memo.dart
+++ lib/models/memo.dart
@@ -1,21 +1,60 @@
 import 'package:meta/meta.dart';
+import 'package:note/models/label.dart';
 
 class Memo {
   final String key;
   String title;
   String body;
+  List<Label> _setLabels;
 
-  Memo({@required this.title, @required this.body, this.key});
-
-  factory Memo.fromMap(Map<String, dynamic> memo) {
-    return Memo(title: memo['title'], body: memo['body'], key: memo['key']);
+  void setLabel(Label label) {
+    _setLabels.add(label);
   }
 
-  Map<String, dynamic> asMap() => {
-        'key':key,
-        'title': title,
-        'body': body,
-      };
+  void removeLabel(Label label) {
+    _setLabels.remove(label);
+  }
+
+  Memo(
+      {@required this.title,
+      @required this.body,
+      List<Label> labels = const <Label>[],
+      this.key})
+      : _setLabels = labels;
+
+  factory Memo.fromMap(Map<String, dynamic> memo) {
+    List<Label> labels = [];
+    if (memo['memo_labels'] != null) {
+      memo['memo_labels'].forEach((key, value) {
+        value['key'] = key;
+        Label label = Label.fromMap((Map<String, dynamic>.from(value)));
+        labels.add(label);
+      });
+    }
+
+    return Memo(
+      key: memo['key'],
+      title: memo['title'],
+      body: memo['body'],
+      labels: labels,
+    );
+  }
+
+  Map<String, dynamic> asMap() {
+    Map<String, dynamic> labels = {};
+    _setLabels.forEach((label) {
+      Map<String, dynamic> mapOfMemo = label.asMap();
+      mapOfMemo.remove('key');
+      labels[label.key] = mapOfMemo;
+    });
+
+    return {
+      'key': key,
+      'title': title,
+      'body': body,
+      'set_labels': labels,
+    };
+  }
 
   @override
   bool operator ==(o) {
