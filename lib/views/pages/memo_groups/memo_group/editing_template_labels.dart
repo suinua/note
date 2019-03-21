c6c27fde062f98478d4bd56c24da7071e7c3b040
suinua <maatinosippo@gmail.com>
2019-03-21 16:59:52 +0900

    ラベル作成機能の追加

--- lib/views/pages/memo_groups/memo_group/editing_template_labels.dart
+++ lib/views/pages/memo_groups/memo_group/editing_template_labels.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:note/blocs/providers/template_memo_labels_bloc_provider.dart';
+import 'package:note/blocs/template_memo_labels_bloc.dart';
 import 'package:note/models/memo_label.dart';
 import 'package:note/views/model_widgets/memo_label.dart';
 
-class EditingTemplateMemoLabelsPage extends StatelessWidget {
+class EditingTemplateMemoLabelsPage extends StatefulWidget {
+  @override
+  _EditingTemplateMemoLabelsPageState createState() =>
+      _EditingTemplateMemoLabelsPageState();
+}
+
+class _EditingTemplateMemoLabelsPageState
+    extends State<EditingTemplateMemoLabelsPage> {
+  TextEditingController _titleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final bloc = TemplateMemoLabelsBlocProvider.of(context);
+
+    Widget _buildLabels(BuildContext context, List<MemoLabel> labels) {
+      return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: labels.length + 1,
+        itemBuilder: (_, int index) {
+          if (index == labels.length) {
+            return GestureDetector(
+              child: Chip(label: Icon(Icons.add)),
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (_) {
+                    return _CreateLabelBottomSheet(
+                        titleController: _titleController, bloc: bloc);
+                  },
+                );
+              },
+            );
+          }
+          return MemoLabelWidget(label: labels[index]);
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -20,11 +55,12 @@ class EditingTemplateMemoLabelsPage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: StreamBuilder<List<MemoLabel>>(
-              builder: (_,AsyncSnapshot<List<MemoLabel>> memoLabels){
+              stream: bloc.getAllLabels,
+              builder: (_, AsyncSnapshot<List<MemoLabel>> memoLabels) {
                 if (memoLabels.hasData) {
-                  return _buildLabels(memoLabels.data);
+                  return _buildLabels(context, memoLabels.data);
                 }
-                return _buildLabels([]);
+                return _buildLabels(context, []);
               },
             ),
           )
@@ -32,13 +68,39 @@ class EditingTemplateMemoLabelsPage extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildLabels(List<MemoLabel> labels) {
-    return ListView.builder(
-      itemCount: labels.length,
-      itemBuilder: (_,int index){
-        return MemoLabelWidget(label: labels[index]);
-      },
+class _CreateLabelBottomSheet extends StatelessWidget {
+  const _CreateLabelBottomSheet({
+    Key key,
+    @required TextEditingController titleController,
+    @required this.bloc,
+  })  : _titleController = titleController,
+        super(key: key);
+
+  final TextEditingController _titleController;
+  final TemplateMemoLabelsBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(child: TextField(controller: _titleController)),
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              bloc.addLabel.add(MemoLabel(
+                title: _titleController.text,
+                color: Colors.red,
+              ));
+            },
+          )
+        ],
+      ),
     );
   }
 }
