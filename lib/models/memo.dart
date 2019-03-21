755a31949e2393d009a0a87022ce0bab6c64a606
suinua <maatinosippo@gmail.com>
2019-03-21 17:08:43 +0900

    不要なコードを削除
    set_labelsはblocで管理するため

--- lib/models/memo.dart
+++ lib/models/memo.dart
@@ -7,50 +7,20 @@ class Memo {
   String title;
   String body;
 
-  List<MemoLabel> _setLabels;
-
-  void setLabel(MemoLabel label) {
-    _setLabels.add(label);
-  }
-
-  void removeLabel(MemoLabel label) {
-    _setLabels.remove(label);
-  }
-
   Memo(
       {@required this.title,
       @required this.body,
-      List<MemoLabel> labels = const <MemoLabel>[],
-      this.key})
-      : _setLabels = labels;
+      this.key});
 
   Memo.fromMap(this.key, Map<String, dynamic> memo) {
-    List<MemoLabel> labels = [];
-    if (memo['memo_labels'] != null) {
-      memo['memo_labels'].forEach((key, value) {
-        value['key'] = key;
-        MemoLabel label = MemoLabel.fromMap((Map<String, dynamic>.from(value)));
-        labels.add(label);
-      });
-    }
-
     this.title = memo['title'];
     this.body = memo['body'];
-    this._setLabels = labels;
   }
 
   Map<String, dynamic> asMap() {
-    Map<String, dynamic> labels = {};
-    _setLabels?.forEach((label) {
-      Map<String, dynamic> mapOfMemo = label.asMap();
-      mapOfMemo.remove('key');
-      labels[label.key] = mapOfMemo;
-    });
-
     return {
       'title': title,
       'body': body,
-      'set_labels': labels,
     };
   }
 
