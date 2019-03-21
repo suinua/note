e538731e3cc53d6893323299ec82278399ea88e7
suinua <maatinosippo@gmail.com>
2019-03-21 15:57:50 +0900

    requiredアノテーションの追加

--- lib/views/model_widgets/memo_label.dart
+++ lib/views/model_widgets/memo_label.dart
@@ -4,7 +4,7 @@ import 'package:note/models/memo_label.dart';
 class MemoLabelWidget extends StatelessWidget {
   final MemoLabel label;
 
-  const MemoLabelWidget({Key key, this.label}) : super(key: key);
+  const MemoLabelWidget({Key key, @required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
