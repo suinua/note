5f94fb4f0a53f25c37a2d581d96064b43dcdb554
suinua <maatinosippo@gmail.com>
2019-03-29 17:58:31 +0900

    onDeleteフィールドの追加

--- lib/views/model_widgets/template_memo_label.dart
+++ lib/views/model_widgets/template_memo_label.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:note/models/template_memo_label.dart';
 
+typedef OnDelete(TemplateMemoLabel label);
+
 class TemplateMemoLabelWidget extends StatelessWidget {
   final TemplateMemoLabel label;
+  final OnDelete onDelete;
 
-  const TemplateMemoLabelWidget({Key key, @required this.label}) : super(key: key);
+  const TemplateMemoLabelWidget({Key key, @required this.label, this.onDelete})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Chip(
+      onDeleted: onDelete == null ? null : () => onDelete(label),
       label: Text(label.title),
       backgroundColor: label.color,
     );
