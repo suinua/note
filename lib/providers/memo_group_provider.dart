8b40f47753969ab1e670d4dbd0560b7326530c16
suinua <maatinosippo@gmail.com>
2019-04-04 17:29:43 +0900

    Add new file `memo_group_provider.dart`

---
+++ lib/providers/memo_group_provider.dart
@@ -0,0 +1,27 @@
+import 'package:bloc_provider/bloc_provider.dart';
+import 'package:flutter/widgets.dart';
+import 'package:note/blocs/memo_group/memo_group_bloc.dart';
+
+class MemoGroupBlocProvider extends BlocProvider<MemoGroupBloc> {
+  MemoGroupBlocProvider({
+    @required MemoGroupBloc value,
+    @required Widget child,
+  }) : super(
+    child: child,
+    creator: (context, _bag) {
+      assert(context != null);
+      return value;
+    },
+  );
+
+
+  MemoGroupBlocProvider.fromBlocContext({
+    @required BuildContext context,
+    @required Widget child,
+  }) : super.fromBlocContext(
+    child: child,
+    context: context,
+  );
+
+  static MemoGroupBloc of(BuildContext context) => BlocProvider.of(context);
+}
