059120391aaa19d073f2a0a301fcfddc60b43497
suinua <maatinosippo@gmail.com>
2019-04-08 21:33:00 +0900

    Add new file

---
+++ lib/providers/memos_bloc_provider.dart
@@ -0,0 +1,24 @@
+import 'package:bloc_provider/bloc_provider.dart';
+import 'package:flutter/widgets.dart';
+import 'package:note/blocs/memo/memos_bloc.dart';
+
+class MemosBlocProvider extends BlocProvider<MemosBloc> {
+  MemosBlocProvider({String groupKey})
+      : super(
+          creator: (context, _bag) {
+            assert(context != null);
+            if (groupKey == null) return null;
+            return MemosBloc(groupKey);
+          },
+        );
+
+  MemosBlocProvider.fromBlocContext({
+    @required BuildContext context,
+    @required Widget child,
+  }) : super.fromBlocContext(
+          child: child,
+          context: context,
+        );
+
+  static MemosBloc of(BuildContext context) => BlocProvider.of(context);
+}
